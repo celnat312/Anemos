d63545d6e4b9c866953ec27f27b3cf612c955f7f
ashley-koh <ashleykoh24@gmail.com>
2024-06-10 16:55:58 +0800

    added basic chat component to dashboard

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,27 +1,42 @@
-import React from "react";
-import Background from "../components/Background";
-import Logo from "../components/Logo";
-import Header from "../components/Header";
-import Paragraph from "../components/Paragraph";
-import Button from "../components/Button";
+import React, { useState, useEffect, useCallback } from "react";
+// import Background from "../components/Background";
+// import Logo from "../components/Logo";
+// import Header from "../components/Header";
+// import Paragraph from "../components/Paragraph";
+// import Button from "../components/Button";
+import { GiftedChat } from "react-native-gifted-chat";
 
 export default function Dashboard({ navigation }) {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages),
+    );
+  }, []);
+
   return (
-    <Background>
-      <Logo />
-      <Header>Welcome 💫</Header>
-      <Paragraph>Congratulations you are logged in.</Paragraph>
-      <Button
-        mode="outlined"
-        onPress={() =>
-          navigation.reset({
-            index: 0,
-            routes: [{ name: "StartScreen" }],
-          })
-        }
-      >
-        Sign out
-      </Button>
-    </Background>
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
   );
 }
